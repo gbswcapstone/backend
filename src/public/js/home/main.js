acf460c763597110fbbd280d8feb41077c42b5a3
wjdgotjd <wjdgotjd529@naver.com>
2023-04-13 20:29:45 +0900

    a

--- src/public/js/home/main.js
+++ src/public/js/home/main.js
@@ -1,16 +1,40 @@
-const textInputField = document.querySelector(".input");
-const btn = document.querySelector(".btn");
-const utterThis = new SpeechSynthesisUtterance();
-const synth = window.speechSynthesis;
+const mapContainer = document.querySelector("#map"),
+  mapOption = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    level: 3,
+  };
 
-const socket = io();
+const map = new kakao.maps.Map(mapContainer, mapOption);
 
-btn.addEventListener("click", () => {
-  socket.emit("ttsSend", { msg: textInputField.value });
-  textInputField.value = "";
-});
+let centerMarker = new kakao.maps.CustomOverlay({});
+let isCenterMarkerDisplay = false;
 
-socket.on("ttsSend", (msg) => {
-  utterThis.text = msg.msg;
-  synth.speak(utterThis);
+const a = navigator.geolocation.watchPosition((position) => {
+  console.log(position);
+  if (isCenterMarkerDisplay) {
+    centerMarker.setMap(null);
+  } else {
+    isCenterMarkerDisplay = true;
+  }
+
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  alert(lat + "\n" + lon);
+
+  const setMap = new kakao.maps.LatLng(lat, lon);
+
+  const makerContent = `
+  <div class="center-marker"></div>
+  `;
+
+  centerMarker = new kakao.maps.CustomOverlay({
+    position: setMap,
+    content: makerContent,
+  });
+
+  centerMarker.setMap(map);
+  console.log(1);
+
+  map.setCenter(setMap);
 });
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
+router.get("/.well-known/pki-validation/:a", (req, res) => {
+  res.sendFile(
+    "C:/Users/User/Desktop/GBSW/capstone/D62111A0205B5C576F0FE89490131FFD.txt"
+  );
+});
 
 router.post("/login", ctrl.process.login);
 router.post("/register", ctrl.process.register);
